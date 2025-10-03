bbb42804f3d4373a0d72fd641592b783d01299a1
Lincoln Madaraka <117019629+Lincoln-Madaraka@users.noreply.github.com>
2025-10-03 23:39:26 +0300

    add to cart

--- app/Http/Controllers/Shop/CartController.php
+++ app/Http/Controllers/Shop/CartController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\Shop;
 
+use App\Models\Product;  
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
@@ -9,15 +10,27 @@ class CartController extends Controller
 {
     public function add(Product $product)
     {
-        $cart = Session::get('cart', []);
+        // simple safe session cart logic (won't exceed stock)
+        $cart = session()->get('cart', []);
 
-        $cart[$product->id] = [
-            'name' => $product->name,
-            'price' => $product->price,
-            'quantity' => ($cart[$product->id]['quantity'] ?? 0) + 1,
-        ];
+        if ($product->stock < 1) {
+            return redirect()->back()->with('error', 'Product is out of stock.');
+        }
 
-        Session::put('cart', $cart);
+        if (isset($cart[$product->id])) {
+            $cart[$product->id]['quantity'] = min($cart[$product->id]['quantity'] + 1, $product->stock);
+        } else {
+            $cart[$product->id] = [
+                'id'       => $product->id,
+                'name'     => $product->name,
+                'price'    => $product->price,
+                'quantity' => 1,
+                'stock'    => $product->stock,
+                'image'    => $product->image,
+            ];
+        }
+
+        session(['cart' => $cart]);
 
         return redirect()->back()->with('success', 'Product added to cart!');
     }

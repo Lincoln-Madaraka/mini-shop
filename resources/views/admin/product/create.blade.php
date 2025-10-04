174b29a68eab4da52224f884eb55182cfa44b05b
Lincoln Madaraka <117019629+Lincoln-Madaraka@users.noreply.github.com>
2025-10-04 22:07:28 +0300

    user product test

--- resources/views/admin/product/create.blade.php
+++ resources/views/admin/product/create.blade.php
@@ -32,7 +32,7 @@
                     </div>
 
                     <div>
-                        <label class="block mb-1 font-semibold">Price ($)</label>
+                        <label class="block mb-1 font-semibold">Price (KES)</label>
                         <input type="number" name="price" value="{{ old('price') }}" class="w-full rounded px-3 py-2 text-black" min="0.01" step="0.01" required>
                         @error('price') <p class="text-red-500 text-sm mt-1">{{ $message }}</p> @enderror
                     </div>
